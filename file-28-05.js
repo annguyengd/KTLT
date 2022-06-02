76f7edce025d43e46a07bee08e69b418a9f3119f
annguyengd <an.nguyen@gooddata.com>
2022-06-02 19:07:44 +0700

    First commit

---
+++ file-28-05.js
@@ -0,0 +1,1 @@
+console.log("Hello class, this is vs code testing");
---
+++ hello-world.js
@@ -0,0 +1,7 @@
+console.log("Hello KTLT class - Welcome");  console.log("xyx"); console.log("Hello KTLT class - Welcome");  console.log("xyx"); console.log("Hello KTLT class - Welcome");  console.log("xyx"); 
+
+ console.log("ABC")
+
+ 
+
+ 
